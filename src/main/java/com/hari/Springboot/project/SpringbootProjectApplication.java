88b86340e364e95015fdaf9096102def5c9de646
TheFirstSaiyan <19hari98@gmail.com>
2022-10-23 22:43:11 +0530

    new commit

---
+++ src/main/java/com/hari/Springboot/project/SpringbootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.hari.Springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hari/Springboot/project/controllers/InitialController.java
@@ -0,0 +1,18 @@
+package com.hari.Springboot.project.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitialController {
+
+    @GetMapping(value = "/")
+    public String initialMethod()
+    {
+        return "You got thissss!";
+    }
+}
